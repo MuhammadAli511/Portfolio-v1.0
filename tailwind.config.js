00777e83f9cd0266757ed18fb41f8ce498b80592
MuhammadAli511 <mohammadali417@outlook.com>
2022-01-31 22:24:27 +0500

    Added Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        c6: '#292935',
+        c12: '#21212b',
+        gradient1: '#38d6e8',
+        gradient2: '#5f95cb',
+        gradient3: '#185ae9',
+        cyanlight: '#38d6e8'
+      }
+    },
+  },
+  plugins: [],
+}
