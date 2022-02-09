939f52f828c0decd53ae0b9ca9346882bd42d78a
MuhammadAli511 <mohammadali417@outlook.com>
2022-02-10 01:06:57 +0500

    Added Skills

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ window.onload = function(){
     const btn2 = document.getElementById('mobile-menu-button2');
     const btn3 = document.getElementById('mobile-menu-button3');
     const btn4 = document.getElementById('mobile-menu-button4');
-    const btn5 = document.getElementById('mobile-menu-button5');
     const btn6 = document.getElementById('mobile-menu-button6');
-    const btn7 = document.getElementById('mobile-menu-button7');
 
     const menu = document.getElementById('mobile-menu');
 
@@ -13,9 +11,7 @@ window.onload = function(){
     btn2.addEventListener("click", unload);
     btn3.addEventListener("click", unload);
     btn4.addEventListener("click", unload);
-    btn5.addEventListener("click", unload);
     btn6.addEventListener("click", unload);
-    btn7.addEventListener("click", unload);
 
     function loadMenu(){
             menu.classList.toggle("-bottom-30");
